514f46f5762030096bf89a77cf0f66eaaf89c5cd
enspdf <sebastianhc1995@hotmail.com>
2015-11-20 16:10:38 -0500

    subida

---
+++ src/main/java/me/shackox/webflow/IncorrectLoginCredentialsException.java
@@ -0,0 +1,7 @@
+package me.shackox.webflow;
+
+public class IncorrectLoginCredentialsException extends Exception{
+	 
+	private static final long serialVersionUID = 3339021692474487380L;
+ 
+}
---
+++ src/main/java/me/shackox/webflow/LoginService.java
@@ -0,0 +1,15 @@
+package me.shackox.webflow;
+
+public class LoginService{
+	 
+	public String performLogin(LoginCredentials loginCredentials) throws IncorrectLoginCredentialsException{
+	 
+		if (loginCredentials.getLoginName() != null && loginCredentials.getLoginName().trim().equalsIgnoreCase("alba") && loginCredentials.getPassword() != null && loginCredentials.getPassword().trim().equalsIgnoreCase("pass")){
+		//user successfully logged in
+			return "success";
+		} else {
+			throw new IncorrectLoginCredentialsException();
+		}
+	}
+ 
+}
